dd57e3d6ef463233aad8165c170689f472691e78
DevExpressExampleBot <fakeEmail>
2022-01-25 21:22:43 +0300

    autogenerate VB

--- VB/MailMergeExample/Program.vb
+++ VB/MailMergeExample/Program.vb
@@ -1,21 +1,18 @@
-﻿Imports DevExpress.Spreadsheet
+Imports DevExpress.Spreadsheet
 Imports System
 
 Namespace MailMergeExample
-    Friend NotInheritable Class Program
 
-        Private Sub New()
-        End Sub
+    Friend Module Program
 
         ''' <summary>
         ''' The main entry point for the application.
         ''' </summary>
-        <STAThread> _
-        Shared Sub Main()
-'            #Region "#main"
-            Dim workbook As Workbook = New DevExpress.Spreadsheet.Workbook()
+        <STAThread>
+        Sub Main()
+#Region "#main"
+            Dim workbook As Workbook = New Workbook()
             workbook.Unit = DevExpress.Office.DocumentUnit.Inch
-
             ' Create a mail merge template.
             Dim template As Worksheet = workbook.Worksheets(0)
             template.Rows(1).RowHeight = 1.5
@@ -39,31 +36,26 @@ Namespace MailMergeExample
             template.Cells("C8").Formula = "=FIELD(""Address"")&"" ""&FIELD(""City"")"
             template.Cells("B9").Value = "About:"
             template.Cells("C9").Formula = "FIELD(""Notes"")"
-
             ' Set a detail range in the template.
             Dim detail As Range = template.Range("C1:C9")
             detail.Name = "DETAILRANGE"
-
             ' Set a header range in the template.
             Dim header As Range = template.Range("B1:B9")
             header.Name = "HEADERRANGE"
-
             ' Switch the mail merge mode to "Multiple Sheets".
             workbook.DefinedNames.Add("MAILMERGEMODE", "=""Worksheets""")
             ' Switch the mail merge mode to "Multiple Documents".
             'workbook.DefinedNames.GetDefinedName("MAILMERGEMODE").RefersTo = "\"Documents\"";
             ' Switch the mail merge mode to "Single Sheet".
             'workbook.DefinedNames.GetDefinedName("MAILMERGEMODE").RefersTo = "\"OneWorksheet\"";
-
             ' Set vertical document orientation.
             workbook.DefinedNames.Add("HORIZONTALMODE", "=TRUE")
-
             ' Perform mail merge.
             workbook.MailMergeDataSource = EmployeeInfo.EmployeesInfo.GetData()
             Dim result = workbook.GenerateMailMergeDocuments()
             result(0).SaveDocument("result.xlsx")
             System.Diagnostics.Process.Start("result.xlsx")
-'            #End Region ' #main
+#End Region  ' #main
         End Sub
-    End Class
+    End Module
 End Namespace
