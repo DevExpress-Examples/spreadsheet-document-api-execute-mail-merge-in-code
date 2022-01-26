721f858dd74166c084bc40546b80afef0729fe1e
AnnaVekhina <42933497+AnnaVekhina@users.noreply.github.com>
2022-01-26 11:48:09 +0300

    Update EmployeeInfo.vb

--- VB/EmployeeInfo/EmployeeInfo.vb
+++ VB/EmployeeInfo/EmployeeInfo.vb
@@ -12,7 +12,7 @@ Namespace EmployeeInfo
         End Sub
 
         Public Shared Function GetData() As IEnumerable(Of EmployeeInfo)
-            Return DataSourceHelper.GetDataSouresFromXml(Of EmployeeInfo)("EmployeeInfo.data.xml", "ArrayOfEmployeeInfo")
+            Return DataSourceHelper.GetDataSouresFromXml(Of EmployeeInfo)("data.xml", "ArrayOfEmployeeInfo")
         End Function
     End Class
 
